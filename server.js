e24773410a34e8227b6e0d19ec968e9257b705bc
Nikhil Talwar <nktalwar15@gmail.com>
2020-08-08 19:29:22 +0530

    Added port as 8080

--- server.js
+++ server.js
@@ -3,5 +3,6 @@ var handleRequest = function(request, response) {
   response.writeHead(200);
   response.end("<h1> Deployed Version is v2.0.</h1>");
 }
+console.log('8080')
 var www = http.createServer(handleRequest);
-www.listen(8081);
+www.listen(8080);
