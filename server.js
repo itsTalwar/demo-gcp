6018a375084241dc1dff3ccffbc2e55f71c21308
Nikhil Talwar <nktalwar15@gmail.com>
2020-08-08 23:26:01 +0530

    Added env

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("<h1> Deployed Version is v5.0.</h1>");
+  response.end(`<h1> Deployed Version is v5.0. ${process.env.NODE_ENV
+  }</h1>`);
 }
 console.log('V5')
 console.log('8080')
