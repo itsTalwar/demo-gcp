89d521977758194d456e56ed2d4ebd371826cdac
Nikhil Talwar <nktalwar15@gmail.com>
2020-08-08 23:33:45 +0530

    Added prod

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
   response.end(`<h1> Deployed Version is v5.0. ${process.env.NODE_ENV
-  }</h1>`);
+  }  --- ${JSON.stringify(process.argv)}</h1>`);
 }
 console.log('V5')
 console.log('8080')
+console.log(JSON.stringify(process.argv))
 var www = http.createServer(handleRequest);
 www.listen(8080);
